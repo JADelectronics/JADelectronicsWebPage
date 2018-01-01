01a185eb1d5735071c8dc60ace443d3388e5a5dc
JADelectronics <JADelectronicArts@gmail.com>
2017-12-31 22:17:50 -0600

    update to disc

--- jadelectronics-webpage-react-client/src/components/buildingBlocks/homePage/HomePageDescriptionComponent.js
+++ jadelectronics-webpage-react-client/src/components/buildingBlocks/homePage/HomePageDescriptionComponent.js
@@ -31,7 +31,7 @@ class HomePageDescriptionComponent extends React.Component {
                     -- 2 --<br/> 99% of the population is afraid to be given a challenge or to challenge themselves…. And it drives me absolutely crazy.<br/>
 
                     <br/><br/>
-                    Now, I can’t change everyone in the world, but maybe I can change the way people see the world. I want people to open their eyes, to see the challenges of everyday life, to see the problems, and do something about it.
+                    I can’t change everyone in the world, but maybe I can change the way people see the world. I want people to open their eyes, to see the challenges of everyday life, to see the problems, and do something about it.
 
                     <br/><br/>
                     You see something wrong that has “always been that way”?<br/><font color="red">Challenge it anyway!</font><br/>
@@ -44,7 +44,7 @@ class HomePageDescriptionComponent extends React.Component {
                     You can achieve anything you want if you put your mind to it, you just have to want it bad enough.
 
                     <br/><br/>
-                    Now, I know quite a few people want to have this mind set but don’t know how to get started. That’s what I am hoping my page will help. Below I have two links, one for the brain, one for the body. As I have gone through life I have faced things that have challenged my brain, body and way of thinking. I have also challenged myself to force myself to grow and improve. The two links below contain the opinions formed and knowledge developed. So go ahead and take a look around, I have everything from forums to tutorials.
+                    I know quite a few people want to have this mind set but don’t know how to get started, the don't know how to force the mental shift. That’s what I am hoping my page will help. Below I have two links, one for the brain, one for the body. As I have gone through life I have faced things that have challenged my brain, body and way of thinking. I have also challenged myself to force myself to grow and improve. The two links below contain the opinions formed and knowledge developed. So go ahead and take a look around, I have everything from forums to tutorials.
 
                     <br/><br/>
                     If you have any questions, comments, or just want to start a conversation in general, don’t be afraid to reach out.
